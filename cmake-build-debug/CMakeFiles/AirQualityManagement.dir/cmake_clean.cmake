20c4df9af13db2a3226fb8ff5524f092f4a7946f
Piotr <pitf99@gmail.com>
2019-12-06 09:34:36 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/AirQualityManagement.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AirQualityManagement.dir/main.cpp.obj"
+  "AirQualityManagement.pdb"
+  "AirQualityManagement.exe"
+  "AirQualityManagement.exe.manifest"
+  "libAirQualityManagement.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AirQualityManagement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
