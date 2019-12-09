2021385a1516d009b0d1263c7d5d254a65275f18
Yuxuan <yuxuan.xie.hit@gmail.com>
2019-12-09 08:55:29 +0100

    Yx:

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,10 +1,23 @@
 #include "../include/airSystem.hpp"
+#include <fstream>
 
 int main(){
 
   airSystem test;
 
+  //relative path is based on the path od makefile.
+  ifstream file("./data/data_10sensors_1year.csv");
 
+  string line;
+
+  if(!file.is_open()){
+    cout << "There is no such file!" << endl;
+    exit(-1);
+  }
+
+  while(getline(file,line)){
+
+  }
 
   return 0;
 
